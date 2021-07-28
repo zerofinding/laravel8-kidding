56bc7230695c6df738b9f59f5297256ff6d332e1
Zero F <71748685+zerofinding@users.noreply.github.com>
2021-07-28 10:54:05 +0700

    Update works.blade.php

--- resources/views/components/works.blade.php
+++ resources/views/components/works.blade.php
@@ -12,7 +12,6 @@
         <p class="sm:text-lg text-md sm:mt-2 mt-1">{{ $field->excerpt }}</p>
 
         <div class="flex-row flex flex-wrap mt-2 gap-2">
-          {{-- {@html handleSplit(field.categories, 'bg-myOrange font-semibold text-white sm:text-md text-md px-3 py-1 rounded-xl border-2 border-b-4 border-black transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-100', field.title, `/works/${{ $field->slug }}`, '#180e07')} --}}
           @foreach(explode(',', $field->categories) as $cat) 
             <div class="bg-myOrange font-semibold text-white sm:text-md text-md px-3 py-1 rounded-xl border-2 border-b-4 border-black transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-100">
               <p class="text-center" style="color:#180e07;">{{ $cat }}</p>
